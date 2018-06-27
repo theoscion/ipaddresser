b3e48b9312eba559a1b1c081fe83bfb77159555c
Christopher Wilbanks <cwilbanks@fender.com>
2018-06-27 12:45:38 -0700

    Defined all structs for parsing the SNS and email JSON configurations, used for notifications

--- notify.go
+++ notify.go
@@ -1,5 +1,34 @@
 package main
 
+type snsAWSJSONConfig struct {
+	AccessKey string `json:"accessKey"`
+	SecretKey string `json:"secretKey"`
+	TopicARN  string `json:"topicARN"`
+}
+
+type snsJSONConfig struct {
+	SNS     snsAWSJSONConfig `json:"sns"`
+	Subject string           `json:"subject"`
+}
+
+type emailSMTPJSONConfig struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type emailRecipient struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type emailJSONConfig struct {
+	SMTP emailSMTPJSONConfig `json:"smtp"`
+	From emailRecipient      `json:"from"`
+	To   []emailRecipient    `json:"to"`
+}
+
 func sendSNSNotification() {
 
 }
